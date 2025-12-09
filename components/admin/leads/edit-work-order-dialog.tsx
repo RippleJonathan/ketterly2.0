199378374b8f02a9bbb138877b4d0ee3897782b8
RippleJonathan <jonathan@rippleroofs.com>
2025-12-08 22:02:07 -0600

    Fix missing DialogDescription import in edit-work-order-dialog

--- components/admin/leads/edit-work-order-dialog.tsx
+++ components/admin/leads/edit-work-order-dialog.tsx
@@ -12,6 +12,7 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogDescription,
 } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
